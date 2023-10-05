f037f4c24f5806a13143d9c383fc1949d8e6130a
IMTREDU <imtr.edu@gmail.com>
2023-10-05 15:32:09 -0700

    Question 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main - It is the entry point
+ *
+ * Return: It will always return 0
+ */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
