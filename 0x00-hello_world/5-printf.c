9ce2d731a0eca784d75e62ff2102a2f6cbd95f97
IMTREDU <imtr.edu@gmail.com>
2023-10-05 15:44:59 -0700

    Number 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - It is the entry point
+ *
+ * Return: It will always return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
