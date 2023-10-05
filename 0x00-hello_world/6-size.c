cce248382e455ecea18b8fdaba9fad1b5413db8f
IMTREDU <imtr.edu@gmail.com>
2023-10-05 15:54:40 -0700

    Question 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - It is the entry point
+ *
+ * Return: It will always return 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a char: %d byte(s)\n", sizeof(int));
+	printf("Size of a char: %d byte(s)\n", sizeof(long int));
+	printf("Size of a char: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a char: %d byte(s)\n", sizeof(float));
+	return (0);
+}
