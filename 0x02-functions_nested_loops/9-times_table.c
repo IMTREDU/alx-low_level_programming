91892893b8ffa39038c718d6668bb085d8d2a1e1
IMTREDU <imtr.edu@gmail.com>
2023-10-10 21:27:47 +0000

    Final commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - It is a function
+ *
+ * Return: Return Void
+ */
+void times_table(void)
+{
+	int i, j, R;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			R = i * j;
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				if (R < 10)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+				_putchar((R / 10) + '0');
+			}
+			_putchar((R % 10) + '0');
+		}
+		if (j != 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		}
+		_putchar('\n');
+	}
+}
