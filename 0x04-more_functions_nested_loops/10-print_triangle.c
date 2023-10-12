be502d7f158084f40eb57a6f765dbba04657204c
IMTREDU <imtr.edu@gmail.com>
2023-10-12 20:16:21 +0000

    Last one hopefully without errors

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,8 +2,28 @@
 /**
  * print_triangle - It is a function
  * @size: Variable from main
- * Return: Return int
+ * Return: Return void
  */
 void print_triangle(int size)
 {
+	int i, j;
 
+	if (size <= 0)
+	{
+		putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = size - 1; j > i; j--)
+		{
+			putchar(' ');
+		}
+		for (j = 0; j <= i; j++)
+		{
+			putchar('#');
+		}
+		putchar('\n');
+	}
+}
