6b88e3705a4a95b5faab68bc388b9541f6ab3eea
IMTREDU <imtr.edu@gmail.com>
2023-10-16 17:27:58 +0000

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - It is a function
+ * @str: Variable from main
+ * Return: Return Void
+ */
+void puts_half(char *str)
+{
+	int i, l;
+
+	for (l = 0; str[l] != '\0'; l++)
+	{
+
+	}
+	if (l % 2 == 0)
+	{
+		for (i = l / 2; str[i] != '\0'; i++)
+		{
+			putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = (l - 1) / 2; str[i] != '\0'; i++)
+		{
+			putchar(str[i]);
+		}
+	}
+	putchar('\n');
+}
