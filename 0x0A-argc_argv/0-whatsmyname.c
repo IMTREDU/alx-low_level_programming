426a6efec75c9ab6ad046236ebe68e0fb7d17fe5
IMTREDU <imtr.edu@gmail.com>
2023-10-27 14:51:49 +0000

    Check

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - It is the main
+ * @argc: Num of argument
+ * @argv: Num of argument(Array)
+ * Return: Return Void
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
