19993f45c89aef00be1947cdee6c53b6fe50dcab
IMTREDU <imtr.edu@gmail.com>
2023-10-31 18:09:19 +0000

    another fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,6 +24,10 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(int));
 		if (grid[i] == NULL)
 		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
