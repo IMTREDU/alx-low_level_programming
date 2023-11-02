fed5f7284b81add4bbca380f16a93e7dca2a492b
IMTREDU <imtr.edu@gmail.com>
2023-11-02 17:28:25 +0000

    Fixt for task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,6 +3,7 @@
  * string_nconcat - It is a function
  * @s1: Variable from main
  * @s2: Variable from main
+ * @n: Variable from main
  * Return: Return str
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -10,40 +11,36 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, len1 = 0, len2 = 0;
 	char *str;
 
-	if (s1 == NULL)
+	if (s1 != NULL)
 	{
-		s1 = "";
+		while (s1[len1] != '\0')
+		{
+			len1++;
+		}
 	}
-	if (s2 == NULL)
+
+	if (s2 != NULL)
 	{
-		s2 = "";
+		while (s2[len2] != '\0')
+		{
+			len2++;
+		}
 	}
-	while (s1[len1])
-	{
-		len1++;
-	}
-	while (s2[len2])
-	{
-		len2++;
-	}
-	if (n >= len2)
-	{
-		n = len2;
-	}
-	str = malloc(sizeof(char) * (len1 + n + 1));
+
+	str = malloc(sizeof(char) * (len1 + (n >= len2 ? len2 : n) + 1));
 	if (str == NULL)
 	{
 		return (NULL);
 	}
+
 	for (i = 0; i < len1; i++)
 	{
 		str[i] = s1[i];
 	}
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n && j < len2; j++, i++)
 	{
-		str[i + j] = s2[j];
+		str[i] = s2[j];
 	}
-	str[i + j] = '\0';
-
+	str[i] = '\0';
 	return (str);
 }
