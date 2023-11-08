aef7a7908a8162447cd4897796b2a35beff80b22
IMTREDU <imtr.edu@gmail.com>
2023-11-08 16:01:49 +0000

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - It is a main
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: Return 0
+ */
+int main(int argc, char *argv[])
+{
+	int n1, n2, result;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+
+	n1 = atoi(argv[1]);
+	op = argv[2];
+	n2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	if ((*op == '/' || *op == '%') && n2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+	result = get_op_func(op)(n1, n2);
+
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - sum of two num
+ * @a: Variable from main
+ * @b: Variable from main
+ * Return: Return sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - sub of two num
+ * @a: Variable from main
+ * @b: Variable from main
+ * Return: Return sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multi of two num
+ * @a: Variable from main
+ * @b: Variable from main
+ * Return: Return multi
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - div of two num
+ * @a: Variable from main
+ * @b: Variable from main
+ * Return: Return div
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - mod of two num
+ * @a: Variable from main
+ * @b: Variable from main
+ * Return: Return mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
