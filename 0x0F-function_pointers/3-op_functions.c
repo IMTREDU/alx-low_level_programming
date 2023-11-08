a166db4ca3ab08c419ddfaae38268d2118a375ec
IMTREDU <imtr.edu@gmail.com>
2023-11-08 21:28:26 +0000

    fix

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,51 +1,71 @@
 #include "3-calc.h"
+
 /**
  * op_add - sum of two num
  * @a: Variable from main
  * @b: Variable from main
  * Return: Return sum
  */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
  * op_sub - sub of two num
  * @a: Variable from main
  * @b: Variable from main
  * Return: Return sub
  */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
  * op_mul - multi of two num
  * @a: Variable from main
  * @b: Variable from main
  * Return: Return multi
  */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
  * op_div - div of two num
  * @a: Variable from main
  * @b: Variable from main
  * Return: Return div
  */
+
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
+
 /**
  * op_mod - mod of two num
  * @a: Variable from main
  * @b: Variable from main
  * Return: Return mod
  */
+
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
