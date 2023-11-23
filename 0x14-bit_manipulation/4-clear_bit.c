01046e17fe5ec0f140f5bbe82c25c15c9441d0d5
IMTREDU <imtr.edu@gmail.com>
2023-11-23 18:12:36 +0000

    First 4 tasks

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - Sets the value of a bit to 0
+ * @n: Pointer to the number
+ * @index: The index of the bit
+ *
+ * Return: 1 if it worked else -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	*n = *n & ~(unsigned long int)(1 << index);
+	return (1);
+}
