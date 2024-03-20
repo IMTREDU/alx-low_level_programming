fb26b956f67d5c67dd61474cfc0f26c95e2c806b
IMTREDU <imtr.edu@gmail.com>
2024-03-20 19:54:09 +0000

    All Tasks

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,13 @@
+#include "m.h"
+/**
+ * main - It is the entry point
+ *
+ * Return: It will always return 0
+ */
+int main(void)
+{
+	print_school();
+	return (EXIT_SUCCESS);
+}
+/* School */
+/* School */
